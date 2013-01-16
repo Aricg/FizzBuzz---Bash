81421d493d42cfb7d31807d29f00645fb730bdf8
Aricg <actual@aricgardner.com>
2013-01-16 13:45:08 -0800

    Create EvenFibonacciNumbers.bash

---
+++ EvenFibonacciNumbers.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+#from http://projecteuler.net/problem=2
+#By considering the terms in the Fibonacci sequence whose values do not exceed four million
+#find the sum of the even-valued terms.
+#
+
+#!/bin/bash
+a=1
+b=1
+final_value=0
+
+fibbo () {
+let a=$(( a + b ))
+let b=$(( a + b ))
+}
+
+is_even()  {
+(! (( "$@" % 2 )) );
+}
+
+nested () {
+if is_even "$i"; then
+        echo "$i" is even;
+        let final_value="$(( final_value + i ))"
+fi
+}
+
+
+while (( "$a" < 4000000 )) || (( "$b" < 4000000 )) ;
+do
+        fibbo "$a" "$b"
+
+                for i in "$a" "$b";
+                        do
+                                nested "$i"
+                        done
+
+((count++))
+done
+        echo Final Value is "$final_value"
