3b9c7ad9335d7b601608a9380951c3e8d54f711a
Aricg <actual@aricgardner.com>
2013-01-24 09:35:00 -0800

    Create is_$1_prime.bash
    Detects if the passed variable "$1" is prime. This should not be done in bash, its very slow. It's an exercise, that's my excuse

---
+++ is_$1_prime.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+#from http://projecteuler.net/problem=3
+
+number="$1"
+number_of_divisors=0
+iteration=1
+
+is_whole()  {
+
+(( "10#$@" == 0 ));
+}
+
+maths (){
+
+result="$(bc <<< "scale=4; $number / $iteration")"
+}
+
+while ((iteration < $1))
+do
+
+maths "$number" "$iteration"
+
+#get remainder
+remainder="${result##*.}";
+
+if is_whole "$remainder"; then
+        echo  "$number" / "$iteration" =  "$result" which is a whole number  
+        let number_of_divisors="$(( number_of_divisors + 1 ))"
+
+fi
+
+((iteration++))
+
+done
+
+if (( number_of_divisors == 2 ));
+        then
+                echo "$number only has "$number_of_divisors" divsors and is therfore is probably prime"
+        else
+                echo "$number has "$number_of_divisors" divsors and is therfore certainly not prime"
+fi
